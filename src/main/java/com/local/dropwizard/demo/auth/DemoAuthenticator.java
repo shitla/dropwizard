e6395a29b99187c509669ee27cadb9c9a178efe0
shitla mishra <shitla mishra>
2018-03-18 17:57:41 +0530

    checking commit

--- src/main/java/com/local/dropwizard/demo/auth/DemoAuthenticator.java
+++ src/main/java/com/local/dropwizard/demo/auth/DemoAuthenticator.java
@@ -1,10 +1,10 @@
 package com.local.dropwizard.demo.auth;
 
+import com.google.common.base.Optional;
+
 import io.dropwizard.auth.AuthenticationException;
 import io.dropwizard.auth.Authenticator;
 import io.dropwizard.auth.basic.BasicCredentials;
-
-import com.google.common.base.Optional;
 
 public class DemoAuthenticator implements Authenticator<BasicCredentials, User> {
 
